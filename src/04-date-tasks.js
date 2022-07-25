0ef510f621e90c4452f4abb18026034499bf0be6
mars <maspam@yandex.ru>
2022-07-25 12:57:45 +0300

    feat: add date solutions

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -33,8 +33,8 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 /**
@@ -51,8 +51,16 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+
+  if (year % 4 !== 0) return false;
+
+  if (year % 100 !== 0) return true;
+
+  if (year % 400 === 0) return true;
+
+  return false;
 }
 
 /**
